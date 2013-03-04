5cb4f000bf02c34bc313c56cbb8fdc7d52aa074b
Bernardo Fontes <falecomigo@bernardofontes.net>
2013-03-04 20:09:29 -0300

    Adiciona configuração inicial do projeto django

---
+++ project/src/settings.py
@@ -0,0 +1,79 @@
+# Django settings for src project.
+from unipath import Path
+from django.conf.global_settings import *
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Bernardo Fontes', 'falecomigo@bernardofontes.net'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': PROJECT_ROOT.child('database.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS = []
+
+TIME_ZONE = 'America/Sao_Paulo'
+LANGUAGE_CODE = 'pt-br'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_ROOT = PROJECT_ROOT.child('media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = PROJECT_ROOT.child('static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_ROOT,
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = '8ji9!x#n44gr=3digcy0g_rujs8og90$!%1$^p9s%+k^v6v_$n'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'src.urls'
+
+TEMPLATE_DIRS = (
+    PROJECT_ROOT.child('templates')
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+)
